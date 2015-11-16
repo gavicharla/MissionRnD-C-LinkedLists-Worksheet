cb5e5e66a0ad68b4fee1f8b2a995f3b86b448ba5
gavicharla <gnssvenkatesh@gmail.com>
2015-11-17 00:46:06 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -20,7 +20,36 @@ struct node {
 	int data;
 	struct node *next;
 };
-
-void sll_012_sort(struct node *head){
+void sll_012_sort(struct node *head)
+{
+	struct node * temp = (struct node *)malloc(sizeof(struct node));
+	temp = head;
+	int count0 = 0, count1 = 0, count2 = 0;
+	while (temp != NULL)
+	{
+		if (temp->data == 0)
+			count0++;
+		else if (temp->data == 1)
+			count1++;
+		else count2++;
+		temp = temp->next;
+	}
+	printf("%d %d %d\n", count0, count1, count2);
+	temp = head;
+	while (count0--)
+	{
+		temp->data = 0;
+		temp = temp->next;
+	}
+	while (count1--)
+	{
+		temp->data = 1;
+		temp = temp->next;
+	}
+	while (count2--)
+	{
+		temp->data = 2;
+		temp = temp->next;
+	}
 	
 }
